9730bea03e1cc4eb91d306feaaa5b73c9e7ba0c0
Ivan Mamani <ivan.mamani@utec.edu.pe>
2019-11-08 09:39:29 -0500

    cc

--- p1-web/static/js/login.js
+++
@@ -1,6 +0,0 @@
-function login(){
-    console.log("Login User");
-    var username = $('#username').val();  //getting username by ID
-    var password = $('#password').val();  // getting password by id
-    console.log("DATA>", username, password);
-}
